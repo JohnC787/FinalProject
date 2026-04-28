7a83814f31c67d4a1e0fcacf5faa87ea0dd26cfa
JohnC787 <johncolligan2005@yahoo.com>
2026-04-27 22:37:01 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,46 +1,52 @@
 public class Player {
-  private String name;
-  private int hits;
-  private int atBats;
-  private int homeRuns;
-  private int strikeOuts;
+    private String name;
+    private int hits;
+    private int atBats;
+    private int homeRuns;
+    private int strikeOuts;
 
-  public Player(String name) {
-    this.name = name;
-    this.hits = 0;
-    this.atBats = 0;
-    this.homeRuns = 0;
-    this.strikeOuts = 0;
-  }
-  public void addAtBat(boolean isHit, boolean isHomeRun, boolean isStrikeout) {
-    atBats++;
+    public Player(String name) {
+        this.name = name;
+        this.hits = 0;
+        this.atBats = 0;
+        this.homeRuns = 0;
+        this.strikeOuts = 0;
+    }
 
-    if (isHit) {
-      hits++;
-      if (isHomeRun) {
-        homeRuns++;
-      }
+    public void addAtBat(boolean isHit, boolean isHomeRun, boolean isStrikeout) {
+        atBats++;
+
+        if (isHit) {
+            hits++;
+            if (isHomeRun) {
+                homeRuns++;
+            }
+        }
+
+        if (isStrikeout) {
+            strikeOuts++;
+        }
     }
-    if (isStrikeout) {
-      strikeOuts++;
+
+    public double getBattingAverage() {
+        if (atBats == 0) return 0.0;
+        return (double) hits / atBats;
     }
-  }
-  public double getBattingAverage() {
-    if (atBats == 0) return 0.0;
-    return (double) hits / atBats;
-  }
-  public String getName() {
-    return name;
-  }
-  public String getStats() {
-    return name + " | AVG: " + String.format("%.3f", getBattingAverage())
-      + " | H: " + hits
-      + " | HR: " + homeRuns
-      + " | SO: " + strikeOuts;
-  }
-  public int getHomeRuns() {
-    return homeRuns;
-  }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHomeRuns() {
+        return homeRuns;
+    }
+
+    public String getStats() {
+        return name + " | AVG: " + String.format("%.3f", getBattingAverage())
+                + " | H: " + hits
+                + " | HR: " + homeRuns
+                + " | SO: " + strikeOuts;
+    }
 }
 
     
